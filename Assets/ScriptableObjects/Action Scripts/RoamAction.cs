ef924f57a9ee2685d3963a829e5793ebdd8f955a
Amy <Amy-LizElizabeth@hotmail.co.uk>
2018-04-09 19:44:30 +0100

    RoamAction now accounts for sight and agent will look for other agents while moving.

--- Assets/ScriptableObjects/Action Scripts/RoamAction.cs
+++ Assets/ScriptableObjects/Action Scripts/RoamAction.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "PluggableAI/Actions/Roam")]
 public class RoamAction : Action
 {
+    Transform target;
 
     public override void Act(StateController stateController)
     {
@@ -17,8 +18,37 @@ public class RoamAction : Action
         controller.navMeshAgent.isStopped = false;
 
         if (controller.navMeshAgent.remainingDistance <= controller.navMeshAgent.stoppingDistance && !controller.navMeshAgent.pathPending)
-       {
+        {
             controller.nextWayPoint = (controller.nextWayPoint + 1) % controller.wayPointList.Count;
+        }
+
+        bool spotted = Look(controller);
+
+        if (spotted)
+        {
+            //persue
+        }
+    }
+
+    private bool Look(StateController controller)
+    {
+
+        RaycastHit hit;
+        float radius = 1f;
+        float range = 10f;
+
+        // so we can see in debug
+        Debug.DrawRay(controller.eyes.position, controller.eyes.forward.normalized * range, Color.green);
+
+        if (Physics.SphereCast(controller.eyes.position, radius, controller.eyes.forward, out hit, range) && ((hit.collider.CompareTag("Timid")) || (hit.collider.CompareTag("Timid"))))
+        {
+            //found target
+            target = hit.transform;
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 }
